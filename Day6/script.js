6e2f95e2a5e36e3cb025affd8e5e19a1fc5b8f15
Smaran <smaranaryal60@gmail.com>
2026-04-28 22:57:26 +0545

    Dom

---
+++ Day6/script.js
@@ -0,0 +1,17 @@
+console.log ("Hello sathi!");
+
+document.firstElementChild.children;
+document.firstChild.parentElement;
+document.firstElementChild.children[1].style.backgroundColor = "white";
+document.body.childNodes[1].childNodes;
+
+let cont = document.body.childNodes[1];
+cont.firstElementChild;
+cont.firstElementChild.style.backgroundColor = "red";
+cont.lastElementChild;
+cont.lastElementChild.style.backgroundColor = "blue";
+
+document.body.firstElementChild.children[3].nextSibling;
+document.body.firstElementChild.children[3].previousSibling;
+document.body.firstElementChild.children[3].nextElementSibling;
+
