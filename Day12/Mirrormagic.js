ad0371728b1b3c5e3abd11718b24f6970686bba9
Smaran <smaranaryal60@gmail.com>
2026-06-01 21:45:34 +0545

    minor change

--- Day12/Mirrormagic.js
+++ Day12/Mirrormagic.js
@@ -1,6 +1,6 @@
 /* The Mirror Mirror: Imagine you have a string, and you need to create a new string that is a mirror image of the original. Write a function that appends the reversed version of the original string to itself. */
 
-function mirrorMirror(str){
+function mirrorMirror(str) {
     return str + str.split("").reverse("").join("")
 }
 
