c3be619a99ca004398e1783f5b6da479fd73add5
Smaran <smaranaryal60@gmail.com>
2026-06-03 18:55:00 +0545

    importing path

---
+++ Day14/aboutpath.js
@@ -0,0 +1,11 @@
+import path from "path"
+
+let mypath = "d:\\JavaScript\\Day14\\aboutpath.js\\smaran.txt"
+
+console.log(path.extname(mypath));
+
+console.log(path.dirname(mypath)); 
+console.log(path.basename(mypath));
+
+console.log(path.join("D:/", "programs\\smaran.txt"));
+
