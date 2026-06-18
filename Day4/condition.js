32576b925b1ed2dc331f354d165c6d95b00749ac
Smaran <smaranaryal60@gmail.com>
2026-06-18 18:31:31 +0545

    if else

---
+++ Day4/condition.js
@@ -0,0 +1,26 @@
+// let age = 29;
+
+// if (age >= 18) {
+//     console.log("Bro You can vote");
+
+// } else {
+
+//     console.log("Bro you cannot vote");
+
+// }
+
+const marks = 80;
+
+if (marks >= 90) {
+    console.log("Excellent bro proud of you");
+} else if (marks >= 80) {
+    console.log("Very good bro proud of you");
+} else if (marks >= 70) {
+    console.log("Good bro you passed");
+} else if (marks >= 60) {
+    console.log("Average bro try harder");
+} else if (marks >= 50) {
+    console.log("Below average bro you need to study");
+} else {
+    console.log("Failed bro you must study hard");
+}
